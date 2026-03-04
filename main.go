8c4aa59ae81234ba2bf37ed9f1205dea9ecbeb80
aritra panda <pandaaritra76@gmail.com>
2026-03-05 03:44:51 +0530

    meh

--- main.go
+++ main.go
@@ -3,9 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
+// data base
+// --------------
+type Store interface {
+	Save(code, uri string)
+	Get(code string) (string, bool)
+}
+type Uristore struct {
+	mu  sync.RWMutex
+	Box map[string]string
+}
+
+// Save and Get
+// -------------
+func (store *Uristore) Save(code, uri string) {
+	store.mu.Lock()
+	store.Box[code] = uri
+	store.mu.Unlock()
+}
+func (store *Uristore) Get(code string) (string, bool) {
+	store.mu.Lock()
+	uri, ok := store.Box[code]
+	store.mu.Unlock()
+	if ok {
+		return uri, ok
+	}
+
+	return "", ok
+
+}
+
+// request handlers
+// ------------------
 func shortner(w http.ResponseWriter, r *http.Request) {
 
 }
