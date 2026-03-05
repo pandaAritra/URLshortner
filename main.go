a7a1e57ebc51d40c06de67c94878a21a2a5cf095
aritra panda <pandaaritra76@gmail.com>
2026-03-05 05:30:11 +0530

    panic

--- main.go
+++ main.go
@@ -1,77 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/pandaAritra/URLshortner/db"
-	"github.com/pandaAritra/URLshortner/tools"
+	"github.com/pandaAritra/URLshortner/routs"
 )
-
-// handler struct
-// --------------
-type Handlers struct {
-	store db.Store //stores the Urls : map
-}
-
-type BigRequest struct {
-	URL string `json:"url"`
-}
-
-type ShortenResponse struct {
-	Code     string `json:"code"`
-	ShortURL string `json:"short_url"`
-}
-
-type ErrorResponse struct {
-	Error string `json:"error"`
-}
-
-// request handlers
-// ------------------
-func (h *Handlers) shortner(w http.ResponseWriter, r *http.Request) {
-	var req BigRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		tools.WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "invalid JSON body"})
-	}
-	if req.URL == "" {
-		tools.WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "Must contain URL"})
-	}
-
-	code := tools.GenerateCode()
-	h.store.Save(code, req.URL)
-
-	tools.WriteJSON(w, http.StatusCreated, ShortenResponse{
-		Code:     code,
-		ShortURL: fmt.Sprintf("http://localhost:8080/%s", code),
-	})
-}
-func (h *Handlers) fetchUrl(w http.ResponseWriter, r *http.Request) {
-	code := r.PathValue("code")
-	if code == "" {
-		tools.WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "Must contain code"})
-	}
-	url, ok := h.store.Fetch(code)
-	if !ok {
-		// code not found — 404
-		tools.WriteJSON(w, http.StatusNotFound, ErrorResponse{Error: "short URL not found"})
-		return
-	}
-	http.Redirect(w, r, url, http.StatusFound)
-}
 
 func main() {
 	store := db.NewUriStore()
-	h := &Handlers{store: store}
+	h := &routs.Handlers{Store: store}
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST /shortner", h.shortner)
+	mux.HandleFunc("POST /shortner", h.Shortner)
 
-	mux.HandleFunc("GET /{code}", h.fetchUrl)
+	mux.HandleFunc("GET /{code}", h.FetchUrl)
 
 	//server config
 
---
+++ routs/routs.go
@@ -0,0 +1,62 @@
+package routs
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/pandaAritra/URLshortner/db"
+	"github.com/pandaAritra/URLshortner/tools"
+)
+
+// handler struct
+// --------------
+type Handlers struct {
+	Store db.Store //stores the Urls : map
+}
+
+type BigRequest struct {
+	URL string `json:"url"`
+}
+
+type ShortenResponse struct {
+	Code     string `json:"code"`
+	ShortURL string `json:"short_url"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+// request handlers
+// ------------------
+func (h *Handlers) Shortner(w http.ResponseWriter, r *http.Request) {
+	var req BigRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		tools.WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "invalid JSON body"})
+	}
+	if req.URL == "" {
+		tools.WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "Must contain URL"})
+	}
+
+	code := tools.GenerateCode()
+	h.Store.Save(code, req.URL)
+
+	tools.WriteJSON(w, http.StatusCreated, ShortenResponse{
+		Code:     code,
+		ShortURL: fmt.Sprintf("http://localhost:8080/%s", code),
+	})
+}
+func (h *Handlers) FetchUrl(w http.ResponseWriter, r *http.Request) {
+	code := r.PathValue("code")
+	if code == "" {
+		tools.WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "Must contain code"})
+	}
+	url, ok := h.Store.Fetch(code)
+	if !ok {
+		// code not found — 404
+		tools.WriteJSON(w, http.StatusNotFound, ErrorResponse{Error: "short URL not found"})
+		return
+	}
+	http.Redirect(w, r, url, http.StatusFound)
+}
